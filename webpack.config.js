f2d98b449368bfabd6b4e26207d2dfb15e717d1f
Xavier Martínez <xavier@xceed.me>
2018-07-21 08:49:51 +0200

    :wrench: Webpack & friends

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  DEPLOY: path.resolve(__dirname, 'dist/js'),
+  SRC: path.resolve(__dirname, 'src'),
+};
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html'),
+      filename: '../index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: paths.DEPLOY,
+    publicPath: '/js',
+  },
+};
